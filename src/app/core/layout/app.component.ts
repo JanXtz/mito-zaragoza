c9d41e763b5f768381b759eeb513c5a0253054f8
JanXtz <ian_lam08@outlook.com>
2025-02-26 20:07:40 -0600

    footer y detalles

--- src/app/core/layout/app.component.ts
+++ src/app/core/layout/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [FooterComponent], // Importamos el Footer
 })
 export class AppComponent {
   title = 'mito-zaragoza';
