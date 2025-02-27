c5e45545fcf9f749ee4638f04a9ce6f2952fd9f2
JanXtz <ian_lam08@outlook.com>
2025-02-26 19:41:15 -0600

    Ligeros detalles aki se trabaja el main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { appRoutes  } from './app/app.routes';
 import { AppComponent } from './app/core/layout/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(appRoutes),
+  ],
+});
